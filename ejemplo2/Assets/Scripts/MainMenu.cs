770ef9447d422dd99d5e07a3735ed7f0c961f811
vicjoel13 <62181926+vicjoel13@users.noreply.github.com>
2020-06-15 20:00:38 -0400

    Game over-scene

--- ejemplo2/Assets/Scripts/MainMenu.cs
+++ ejemplo2/Assets/Scripts/MainMenu.cs
@@ -6,7 +6,11 @@ public class MainMenu : MonoBehaviour
 {
    public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(1);
+    }
+    public void Play_more_no()
+    {
+        SceneManager.LoadScene(0);
     }
     public void QuitGame()
     {
